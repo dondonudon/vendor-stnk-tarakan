0612575344d0a3d64d359b19226b7fe7e5f03c5c
Laurentius Kevin Hendrawanto <45349515+LaurentiusKevin@users.noreply.github.com>
2019-12-11 14:54:12 +0700

    0.8.6 - Menampilkan Nominal di PO baru

--- resources/views/dashboard/_partials/footer.blade.php
+++ resources/views/dashboard/_partials/footer.blade.php
@@ -5,7 +5,7 @@
         Developed by <a href="https://waveitsolution.com/">{{ config('app.developer') }}</a>
     </div>
     <div class="footer-right">
-        <div class="text-dark" data-toggle="tooltip" data-placement="top" data-html="true" title="<em>LastUpdate:</em><br> {{ config('app.lastupdate') }}">
+        <div class="text-dark" data-toggle="tooltip" data-placement="top" data-html="true" title="<em>Last Update:</em><br> {{ config('app.lastupdate') }}">
             version: {{ config('app.version') }}
         </div>
     </div>
